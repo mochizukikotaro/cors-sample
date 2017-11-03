d028f345bce6e11134d745401fecd2bf2d97afc5
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-11-03 17:49:44 +0900

    Allow methods and headers

--- server.go
+++ server.go
@@ -15,10 +15,10 @@ type Ping struct {
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 
-	if (r.Method != http.MethodGet) {
-		http.Error(w, "GET 以外です", http.StatusInternalServerError)
-		return
-	}
+	//if (r.Method != http.MethodGet) {
+	//	http.Error(w, "GET 以外です", http.StatusInternalServerError)
+	//	return
+	//}
 
 	dump, err := httputil.DumpRequest(r, true)
 	if err != nil {
@@ -30,6 +30,18 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	ping := Ping{http.StatusOK, "root"}
 	res, _ := json.Marshal(ping)
 	w.Header().Set("Content-Type", "application/json")
+
+	// ひとつめの学び
+	// これを返せば、GET はできる、POST もできる
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// これを追加すると POST with application/json が可能となる
+	w.Header().Set("Access-Control-Allow-Methods","POST, GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers",
+		"Content-Type, X-CORS-Sample")
+
+	// さらに、カスタムヘッダを追加する
+
 	w.Write(res)
 
 }
